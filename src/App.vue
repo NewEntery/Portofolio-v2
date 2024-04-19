0a130d7730e832722a96a3a5001e3ffa52bda1e2
MUHAMMAD RIZKI HERDIAN <54704712+NewEntery@users.noreply.github.com>
2024-04-19 10:44:53 +0700

    finalproject

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
     <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <button @click="redirectToHome" class="flex">
-          <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">bagasRakha();</span>
+          <span class="self-center text-lg text-[#4d6ee6] font-semibold whitespace-nowrap fadein-bot hover:text-sky-400">MR.Herdian</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://github.com/rakha-elctrnx"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
+          <a href="https://www.instagram.com/mr.herdian1/" target="_blank" rel="noopener noreferrer"><img class="w-9 rounded-full mr-4" src="https://seeklogo.com/images/I/instagram-new-2016-logo-D9D42A0AD4-seeklogo.com.png" alt="instagram"></a>
+          <a href="https://www.instagram.com/mr.herdian1/" target="_blank" rel="noopener noreferrer"><h3 class="mt-1 text-[#4d6ee6]">ORDER NOW</h3></a>
+          
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
@@ -18,18 +20,12 @@
                 class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
                 aria-current="page">Home</router-link>
             </li>
-            <li>
-              <router-link to="/about"
-                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
-            </li>
+              
             <li>
               <router-link to="/portfolio"
                 class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
             </li>
-            <li>
-              <router-link to="/blog"
-                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
-            </li>
+            
           </ul>
         </div>
       </div>
@@ -42,9 +38,9 @@
   <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
   <nav class="flex justify-around py-4 text-xs">
     <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
-    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
+    
     <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
-    <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
+    
   </nav>
 </footer>
 </template>
@@ -79,7 +75,7 @@ export default {
 }
 
 ::-webkit-scrollbar-thumb {
-  background: #ffdb70;
+  background: #4d6ee6;
   border-radius: 5px;
 }
 
@@ -119,7 +115,7 @@ nav a.router-link-exact-active::after {
   width: 100%;
   height: 4px;
   border-radius: 2px;
-  background-color: #ffdb70;
+  background-color: #4d6ee6;
 }
 
 nav a.router-link-exact-active:hover {
@@ -137,5 +133,17 @@ nav a.router-link-exact-active:hover {
     transform: translateX(0);
   }
 }
+a {
+  cursor: pointer; /* Ini sebenarnya tidak perlu karena default <a> sudah pointer */
+}
 
+/* Mengubah warna saat di-hover */
+a:hover h3 {
+  color: #6b93d6; /* Warna lebih muda dari #4d6ee6 */
+}
+
+/* Opsi lain, jika Anda ingin mengubah warna ikon Instagram saat di-hover */
+a:hover img {
+  filter: brightness(1.2); /* Membuat gambar sedikit lebih terang */
+}
 </style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,8 +6,8 @@ export default {
       items: [
         {
           id: 1,
-          name: 'Personal Website',
-          imageUrl: 'portfolio_v2',
+          name: 'Photo 1',
+          imageUrl: '1',
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
@@ -15,8 +15,8 @@ export default {
         },
         {
           id: 2,
-          name: 'Sahabat Muslim',
-          imageUrl: 'sahabatmuslim',
+          name: 'Photo 2',
+          imageUrl: '2',
           status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/rakha-elctrnx/muslim-companion',
@@ -24,8 +24,8 @@ export default {
         },
         {
           id: 3,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
+          name: 'Photo 3',
+          imageUrl: '3',
           status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
           tech: 'Laravel 9, Bootstrap',
           github: 'null',
@@ -33,8 +33,8 @@ export default {
         },
         {
           id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
+          name: 'Photo 4',
+          imageUrl: '4',
           status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
           tech: 'Laravel 8, Bootstrap',
           github: 'null',
@@ -42,100 +42,230 @@ export default {
         },
         {
           id: 5,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
+          name: 'Photo 5',
+          imageUrl: '5',
           status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
           tech: 'Codeigniter 3, Bootstrap',
           github: 'null',
           demo: 'null'
+        },
+        {
+          id: 6,
+          name: 'Photo 6',
+          imageUrl: '6',
+          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
+          tech: 'VueJS 3, Tailwind',
+          github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
+          demo: 'https://bagasrakha.netlify.app/'
+        },
+        {
+          id: 7,
+          name: 'Photo 7',
+          imageUrl: '7',
+          status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
+          tech: 'VueJS 3, Tailwind',
+          github: 'https://github.com/rakha-elctrnx/muslim-companion',
+          demo: 'https://sahabat-muslim.netlify.app/'
+        },
+        {
+          id: 8,
+          name: 'Photo 8',
+          imageUrl: '8',
+          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
+          tech: 'Laravel 9, Bootstrap',
+          github: 'null',
+          demo: 'null'
+        },
+        {
+          id: 9,
+          name: 'Photo 9',
+          imageUrl: '9',
+          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
+          tech: 'Laravel 8, Bootstrap',
+          github: 'null',
+          demo: 'null'
+        },
+        {
+          id: 10,
+          name: 'Photo 10 ',
+          imageUrl: '10',
+
+        },
+
+        {
+          id: 12,
+          name: 'Photo 11 ',
+          imageUrl: '12',
+
+        },
+        {
+          id: 13,
+          name: 'Photo 12 ',
+          imageUrl: '13',
+
         }
-      ]
+
+      ],
+      showModal: false,
+      isLoading: false,
+      selectedImageUrl: null,
     };
+
+  },
+  
+  methods: {
+    openModal(imageUrl) {
+      this.isLoading = true; // Aktifkan loading
+      this.selectedImageUrl = imageUrl;
+      this.showModal = true;
+    },
+    imageLoaded() {
+      this.isLoading = false; // Nonaktifkan loading setelah gambar dimuat
+    }
   }
 }
 </script>
 <template>
   <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
+    <div v-if="showModal" class="modal" @click="showModal = false">
+    <div class="modal-content" @click.stop>
+      <span class="close" @click="showModal = false">&times;</span>
+      <!-- Tampilkan loading saat isLoading true -->
+      <div v-if="isLoading" class="loading-overlay">
+    <div class="loading-spinner">Loading...</div>
+  </div>
+      <!-- Gunakan event @load pada img untuk memanggil imageLoaded -->
+      <img :src="selectedImageUrl" alt="Selected Image" class="modal-image" @load="imageLoaded">
+    </div>
+  </div>
     <article data-page="about">
       <header>
         <div
           class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
           <!-- <div class="h-[1px] w-10 bg-amber-200 md:w-20 aos-init aos-animate"></div> -->
-          <h4>Past Project Experience</h4>
-          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300">
-            Explore the projects I've worked on so far</h4>
+          <h4>RESULT MY PHOTO</h4>
+          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-blue-500">
+            Explore the result Photo I've worked on so far</h4>
         </div>
       </header>
       <section>
         <div>
-          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+          
+          <div
+            class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
             <div v-for="item in items" :key="item.id">
               <div
-                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
-                <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
-                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
-                    :src="'/img/portfolio-' + item.imageUrl + '.png'">
+                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
+                <div class="flex items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
+                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded-xl"
+                    :src="'/img/' + item.imageUrl + '.png'" @click="openModal('/img/' + item.imageUrl + '.png')">
                 </div>
+
                 <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
                   <div class="title-text font-medium text-secondary">{{ item.name }}
                   </div>
-                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
-                  <div class="w-full mt-4 text-normal text-sm text-left text-amber-200">
-                    {{ item.tech }}
-                  </div>
+
                   <div class="w-full flex justify-end">
-                    <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'"
-                        :href="item.github" target="_blank" rel="noreferrer"
-                        title="View github repository" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-                          </path>
-                        </svg></a>
-                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
-                        title="View finished project" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                          <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg></a></div>
+
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </article>
-</div></template>
+      </section>
+    </article>
+  </div>
+</template>
 
 <style>
 .item-card:hover {
   transition: transform 0.3s ease;
   transform: translateY(-8px);
 }
-svg:hover{
+
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  position: relative;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-image {
+  max-width: 100%;
+  height: auto;
+}
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%; /* Sesuaikan dengan kebutuhan */
+}
+
+svg:hover {
   stroke: #ffdb70;
 }
+
 @keyframes fadeZoomIn {
   0% {
     opacity: 0;
     transform: scale(0.5);
   }
+
   100% {
     opacity: 1;
     transform: scale(1);
   }
 }
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.loading-spinner {
+  /* Sesuaikan dengan kebutuhan */
+  color: white;
+  font-size: 20px;
+}
+img:hover {
+  cursor: pointer;
+}
 /* Menggunakan animasi pada elemen yang diinginkan */
 .fade-zoom-in {
   animation: fadeZoomIn 1s ease-in-out;
-}
-</style>
+}</style>
